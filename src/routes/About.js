42c2d4f30ae4cb4dfecb2bad202598631c272fab
jiwon9426 <jiwon9426@korea.ac.kr>
2020-09-21 23:13:29 +0900

    최종완성코드

--- src/routes/About.js
+++ src/routes/About.js
@@ -5,8 +5,8 @@ function About() {
     return ( <
         div className = "about__container" >
         <
-        span > About this page: I built it because I love movies. < /span> <
-        span > 박지원 < /span> < /
+        span > "About this page: I built it because I love movies. But it is a test page" < /span> <
+        span > -박지원 < /span> < /
         div > );
 }
 
