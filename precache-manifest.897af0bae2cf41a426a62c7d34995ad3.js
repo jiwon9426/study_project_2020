fbb2d4304cdf26637e5802a54c55f8c990bcc21b
jiwon9426 <jiwon9426@korea.ac.kr>
2020-09-21 23:20:49 +0900

    Updates

---
+++ precache-manifest.897af0bae2cf41a426a62c7d34995ad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f88f26d53bc860fdf45f1bbaca3ab83",
+    "url": "/study_project_2020/index.html"
+  },
+  {
+    "revision": "45abd116bb778e5a57fa",
+    "url": "/study_project_2020/static/css/main.61def0a9.chunk.css"
+  },
+  {
+    "revision": "35f1acf7950510af1491",
+    "url": "/study_project_2020/static/js/2.721c03ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/study_project_2020/static/js/2.721c03ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45abd116bb778e5a57fa",
+    "url": "/study_project_2020/static/js/main.1a3ed100.chunk.js"
+  },
+  {
+    "revision": "064c9ad714ac521b4e3b",
+    "url": "/study_project_2020/static/js/runtime-main.a2a77c0b.js"
+  }
+]);
