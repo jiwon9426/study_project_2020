f2395199c978a7d31fbe2b6bde35ab59f04892e4
jiwon9426 <jiwon9426@korea.ac.kr>
2020-09-21 22:32:46 +0900

    최종완성코드

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return ( <
+        div className = "about__container" >
+        <
+        span > About this page: I built it because I love movies. < /span> <
+        span > 박지원 < /span> < /
+        div > );
+}
+
+export default About;
